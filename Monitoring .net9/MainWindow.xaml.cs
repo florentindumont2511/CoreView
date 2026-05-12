2f6cdb6bdfa88a92637d75f0f70b0cb960a62945
Florentin Dumont <f.dumont395@laposte.net>
2026-05-12 11:09:44 +0200

    Ajout d'un graphique pour température GPU sur 60 s puis ajout de l'heure et la date.

--- Monitoring .net9/MainWindow.xaml.cs
+++ Monitoring .net9/MainWindow.xaml.cs
@@ -3,6 +3,10 @@ using System;
 using System.Windows;
 using System.Windows.Threading;
 using System.Windows.Input;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace Monitoring_net9
 {
@@ -10,17 +14,31 @@ namespace Monitoring_net9
     {
         private readonly HardwareMonitorService monitorService;
         private readonly DispatcherTimer timer;
+        public ISeries[] GpuTempSeries { get; set; }
+
+        private ObservableCollection<double> gpuTempValues = new();
+
 
         public MainWindow()
         {
             InitializeComponent();
 
             monitorService = new HardwareMonitorService();
+            GpuTempSeries =
+            [
+                new LineSeries<double>
+                {
+                    Values = gpuTempValues
+                }
+            ];
+
+            DataContext = this;
 
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_Tick;
             timer.Start();
+
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -35,6 +53,12 @@ namespace Monitoring_net9
 
         private void Timer_Tick(object? sender, EventArgs e)
         {
+            TimeText.Text = DateTime.Now.ToString("HH:mm:ss");
+
+            DateText.Text = DateTime.Now.ToString(
+                "dddd dd MMMM yyyy",
+                new CultureInfo("fr-FR"));
+
             monitorService.Update();
 
             CpuUsageText.Text =
@@ -48,6 +72,13 @@ namespace Monitoring_net9
 
             GpuTempText.Text =
                 $"{monitorService.GpuTemp:F1} °C";
+
+            gpuTempValues.Add(monitorService.GpuTemp);
+
+            if (gpuTempValues.Count > 60)
+            {
+                gpuTempValues.RemoveAt(0);
+            }
 
             GpuMemoryText.Text =
                 $"{monitorService.GpuMemoryUsedGB:F1} GB";
